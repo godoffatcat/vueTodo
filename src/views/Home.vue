a8e56ed5ccb59d06106f2c65704dd4e1dfa89d0c
fatcat <747061165@qq.com>
2020-07-16 11:33:59 +0800

    props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,77 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div class='titleAndInput'>
+            <div class="titleText">T o d o ?</div>
+            <a-input class="input" v-on:input='todoInput' v-bind:value='inputTodo' placeholder='what could you want to do?' style="height: 80px; width: 450px; align-self: center; font-size: 30px;"/>
+            <div class="button">
+                <a-button class='add' v-on:click='addToList' type="primary" style="height: 70px; width: 160px; align-self: center; font-size: 30px;">Add</a-button>
+            </div>
+            <div class='icon'>
+                <img src="../components/mao.png" style="width: 200px; height: 200px;">
+            </div>
+          </div>
+          <todoList v-bind:list='todoTotheList' />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    import todoList from './sonComp/todoList.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    const beforeCreate  = () => {
+        document.querySelector('body').setAttribute('style', 'background-color:#ffc0cb;')
+    }
+    beforeCreate ()
+
+    export default {
+        name: "Home",
+        data() {
+            return {
+                placeholderText: 'what could you want to do?',
+                inputTodo: '',
+                todoTotheList: [],
+            }
+        },
+        methods: {
+            todoInput: function(event) {
+                let value = event.target.value
+                this.inputTodo = value
+                console.log('value is ', value)
+            },
+            addToList: function() {
+                let todo = this.inputTodo
+                if(todo === '') {
+                    alert('plz input')
+                }
+                var list = this.todoTotheList
+                list.push(todo)
+                this.inputTodo = ''
+                console.log('todo is', this.todoTotheList)
+            }
+        },
+        components: {
+            todoList,
+        },
+    }
 </script>
+
+<style>
+    .titleAndInput {
+        width: 60%;
+        height: 100vh;
+        border-right: 2px rgb(242, 243, 241) solid;
+        display: flex;
+        flex-direction: column;
+    }
+    .titleText {
+        font-size: 100px;
+        font-family: 'Gill Sans';
+        margin-top: 190px;
+        margin-bottom: 20px;
+    }
+    .button {
+        margin-top: 50px;
+    }
+    .icon {
+        text-align: left;
+    }
+</style>
---
+++ src/views/sonComp/todoList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="listArea">
+        <div class="listTitle"> TODO LIST </div>
+            <h3 v-if='empty'>空空如也...</h3>
+            <li v-for='single in todoTotheList'></li>
+            <!-- <singleTodo v-if='todo'></singleTodo> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'todoList',
+        data: function() {
+            return {
+                empty: true,
+            }
+        },
+        props: [
+            'todoTotheList'
+        ],
+        methods: {
+            listContentBy: function() {
+                if (!todoTotheList) {
+                this.empty= true
+                } else {
+                    this.empty = false
+                }
+            },
+        },
+        template: `
+        <div class='singleTodo'>
+            <div class='todoContent' v-bind:value='todos'></div>
+        </div>
+        `
+    }
+</script>
+
+<style>
+    .listArea {
+        width: 30%;
+        height: 60vh;
+        text-align: right;
+    }
+</style>
